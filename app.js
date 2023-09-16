6e3ee285558f57e498cb4653434dd04d0ef7a21c
ahadhan <ahadkhanmay@gmail.com>
2023-09-17 01:17:14 +0500

    redirecting to dashboard

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 let login = document.getElementById("login");
 let signUp = document.getElementById("signup");
 let errorsAlert = document.getElementById("alert");
-let alertText = document.getElementById("alert-text")
+let alertText = document.getElementById("alert-text");
+let userInNav = document.getElementById("user-in-nav");
+
 
 let email = document.getElementById("exampleInputEmail1");
 let password = document.getElementById("exampleInputPassword1");
@@ -16,42 +18,46 @@ let passwordCA = document.getElementById("exampleInputPassword2");
 
 
 
-const changePagetoCA = () => {
-    event.preventDefault()
-    // console.log(login);
+const changePagetoCA = (e) => {
+    e.preventDefault()
+    console.log(login);
     login.style.display = "none";
     signUp.style.display = "block";
 }
 
-const changePagetoLogin = () => {
-    event.preventDefault()
+const changePagetoLogin = (e) => {
+    e.preventDefault()
     signUp.style.display = "none";
     login.style.display = "block";
 }
 
+let usersData = JSON.parse(localStorage.getItem('usersData')) || [];
+
+
+
+if(login){
 login.addEventListener("submit", (event) => {
     event.preventDefault();
     validateInputs();
-    // if(messages <= 0){
-    //     msg= "Successfully Login!.";
-    //     messages.push(msg);
-    //     errorsM();
-    // // event.reset();
-    //     }
-});
+})};
 
+if(signUp){
 signUp.addEventListener("submit", (event) => {
     event.preventDefault();
     validateInputs();
-    // if(messages <= 0){
-    //     msg= "Successfully Create Account!";
-    //     messages.push(msg);
-    //     errorsM();
-    // // event.reset();
-    //     }
-});
+})};
+
+
+
 
 var messages = [];
+var currentUser = localStorage.getItem("currentUser") || null;
+var userId = 1000;
+var finalUserId;
+var userFound;
+
+
+
 
 
 const validateInputs = () => {
@@ -62,7 +68,8 @@ const validateInputs = () => {
     const emailValue2 = email2.value;
     const confirmValue = confirmPassword.value;
     const passwordCa = passwordCA.value;
-    console.log(emailValue, passwordValue,);
+
+    // console.log(emailValue, passwordValue,);
     // if()
 
     // if(login.style.display === "block" ){
@@ -71,7 +78,7 @@ const validateInputs = () => {
         messages.push(msg);
         console.log(messages.length)
         errorsM();
-        console.log("email checked")
+        console.log(email)
     }
     else if (passwordValue.length < 8 || passwordValue.length === 0) {
         msg = "Password Should contain atleast 8 characters";
@@ -81,11 +88,30 @@ const validateInputs = () => {
         console.log("password checked")
     }
     else {
-        msg = "Successfully Login!"
-        messages.push(msg);
-        login.reset();
-        errorsM();
+        const isRegistered = (emailValue, passwordValue) => {
+            return usersData.find(user => user.email2 === emailValue && user.passwordCA === passwordValue);
+        }
+        userFound = isRegistered(emailValue, passwordValue)
+        if (userFound) {
+            msg = "Successfully Login!"
+            messages.push(msg);
+            login.reset();
+            errorsM();
+            console.log(emailValue, passwordValue, userFound)
+            const currenUserfn =  () => {
+                localStorage.setItem('currentUser', JSON.stringify(userFound));
+                window.location.href = 'home.html';
+            };
+            currenUserfn();
+        }
+        else {
+            msg = "Your are not registered. Create account to get registered!"
+            messages.push(msg);
+            errorsM();
+        }
     }
+
+   
 
     if (signUp.style.display === "block") {
 
@@ -97,7 +123,7 @@ const validateInputs = () => {
             console.log("first name checked")
         }
 
-        else if (emailValue2 === "" || emailValue2 === null) {
+        if (emailValue2 == "" || emailValue2 == null) {
             msg = "Email Cant be Blank!"
             messages.push(msg);
             console.log(messages.length)
@@ -123,7 +149,29 @@ const validateInputs = () => {
             messages.push(msg);
             signUp.reset();
             errorsM();
+
+            const postData = () => {
+                if (usersData && Array.isArray(usersData) && usersData.length > 0) {
+                    lastUserId = (usersData[usersData.length - 1]).userId + 1;
+                    finalUserId = lastUserId;
+                }else{
+                    finalUserId = userId;
+                }
+
+                const userData = {
+                    email2: emailValue2,
+                    passwordCA: passwordCa,
+                    firstName: firstNameValue,
+                    userId: finalUserId
+                }
+                usersData.push(userData)
+                localStorage.setItem('usersData', JSON.stringify(usersData));
+                //   console.log(JSON.stringify(localStorage.getItem("userData")));
+            }
+            postData();
+            window.location.href = 'home.html';
         }
+
     }
 }
 
@@ -156,3 +204,75 @@ const showPassword = () => {
 //     // event.default();
 //     }
 // }
+
+// redirecting to Dashboard 
+// TODO SECTION
+
+if(userInNav){
+    let userInNav = document.getElementById("user-in-nav");
+  
+    if (currentUser) {
+      userInNav.innerText = "Welcome " + JSON.parse(currentUser).firstName;
+    } else {
+      userInNav.innerText = "User";
+    }}
+
+        let ulElem = document.getElementsByClassName("ul");
+        let inputValue = document.getElementById("input");
+        var todoId;
+        let todoList = document.getElementById("todoList");
+
+    let todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+
+    function updateTodoList() {
+        todoList.innerHTML = "";
+        todoItems.forEach((todo, index) => {
+            const li = document.createElement("button");
+            li.setAttribute("class", "btn btn-success")
+            li.textContent = todo.todoItem;
+
+            const deleteButton = document.createElement("span");
+                deleteButton.innerHTML = '<i class="fa-solid fa-trash" style="color: #ed3838;"></i>'
+                deleteButton.onclick = () => deleteTodo(index);
+            
+                li.appendChild(deleteButton);
+                todoList.appendChild(li);
+        });
+    }
+
+    function addTodo() {
+        const todoItem = inputValue.value.trim();
+        todoId = 100;
+        
+        
+        if (todoItem !== "") {
+            if(Array.isArray(todoItems) && todoItems.length > 0){
+                todoId = todoItems[todoItems.length - 1].todoId + 1;
+        }
+        const newTodo = {
+            todoItem,
+            todoId,
+            userId: JSON.parse(currentUser).userId
+        }
+        console.log();
+            todoItems.push(newTodo);
+            localStorage.setItem("todoItems", JSON.stringify(todoItems));
+            inputValue.value = "";
+            updateTodoList();
+        }
+    }
+    function deleteTodo(index) {
+        todoItems.splice(index, 1);
+        localStorage.setItem("todoItems", JSON.stringify(todoItems));
+        updateTodoList();
+    }
+
+    function deleteAll (){
+        todoItems.length = 0;
+        localStorage.removeItem("todoItems")
+        updateTodoList();
+        }
+
+    // todoItems? todoItems.map(item, (item) => {
+    //     console.log(item);
+    // })
