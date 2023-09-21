7b2b3343342e7676f00cd6a0ef8c7345e77344c2
ahadhan <ahadkhanmay@gmail.com>
2023-09-22 00:54:07 +0500

    fixing filter todo bug

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let signUp = document.getElementById("signup");
 let errorsAlert = document.getElementById("alert");
 let alertText = document.getElementById("alert-text");
 let userInNav = document.getElementById("user-in-nav");
+let deleteAllBtn = document.getElementById("delete-all");
+let showAllBtn = document.getElementById("show-all");
 
 
 let email = document.getElementById("exampleInputEmail1");
@@ -35,17 +37,19 @@ let usersData = JSON.parse(localStorage.getItem('usersData')) || [];
 
 
 
-if(login){
-login.addEventListener("submit", (event) => {
-    event.preventDefault();
-    validateInputs();
-})};
+if (login) {
+    login.addEventListener("submit", (event) => {
+        event.preventDefault();
+        validateInputs();
+    })
+};
 
-if(signUp){
-signUp.addEventListener("submit", (event) => {
-    event.preventDefault();
-    validateInputs();
-})};
+if (signUp) {
+    signUp.addEventListener("submit", (event) => {
+        event.preventDefault();
+        validateInputs();
+    })
+};
 
 
 
@@ -55,7 +59,7 @@ var currentUser = localStorage.getItem("currentUser") || [];
 var userId = 1000;
 var finalUserId;
 var userFound;
-
+var newTodo;
 
 
 
@@ -93,16 +97,21 @@ const validateInputs = () => {
         }
         userFound = isRegistered(emailValue, passwordValue)
         if (userFound) {
+            console.log("User Found:", userFound);
             msg = "Successfully Login!"
             messages.push(msg);
             login.reset();
             errorsM();
             console.log(emailValue, passwordValue, userFound)
-            const currenUserfn =  () => {
-                localStorage.setItem('currentUser', JSON.stringify(userFound));
-                window.location.href = 'home.html';
-            };
-            currenUserfn();
+            localStorage.setItem('currentUser', JSON.stringify(userFound));
+            changePage();           
+            // window.location.href = 'home.html';
+            
+            //  const updateCurrList=()=>{  
+            //     localStorage.setItem("todoItems", JSON.stringify(todoItems));
+            //     updateTodoList();
+            // }
+            // updateCurrList();
         }
         else {
             msg = "Your are not registered. Create account to get registered!"
@@ -111,7 +120,7 @@ const validateInputs = () => {
         }
     }
 
-   
+
 
     if (signUp.style.display === "block") {
 
@@ -154,7 +163,7 @@ const validateInputs = () => {
                 if (usersData && Array.isArray(usersData) && usersData.length > 0) {
                     lastUserId = (usersData[usersData.length - 1]).userId + 1;
                     finalUserId = lastUserId;
-                }else{
+                } else {
                     finalUserId = userId;
                 }
 
@@ -167,15 +176,17 @@ const validateInputs = () => {
                 usersData.push(userData)
                 localStorage.setItem('usersData', JSON.stringify(usersData));
                 //   console.log(JSON.stringify(localStorage.getItem("userData")));
-                const currenUserfn = () => {
-                    localStorage.setItem('currentUser', JSON.stringify(userData));
-                    // window.location.href = 'home.html';
-                };
-            currenUserfn();
+                localStorage.setItem('currentUser', JSON.stringify(userData));
+                // localStorage.setItem("todoItems", JSON.stringify(todoItems));
             }
             postData();
-            
             window.location.href = 'home.html';
+            // todoItems.push(newTodo);
+            const updateCurrList=()=>{  
+                localStorage.setItem("todoItems", JSON.stringify(todoItems));
+                updateTodoList();
+            }
+            updateCurrList();
         }
 
     }
@@ -214,71 +225,138 @@ const showPassword = () => {
 // redirecting to Dashboard 
 // TODO SECTION
 
-if(userInNav){
+if (userInNav) {
     let userInNav = document.getElementById("user-in-nav");
-  
+
     if (currentUser) {
-      userInNav.innerText = "Welcome " + JSON.parse(currentUser).firstName;
+        userInNav.innerText = "Welcome " + JSON.parse(currentUser).firstName;
     } else {
-      userInNav.innerText = "User";
-    }}
+        userInNav.innerText = "User";
+    }
+}
 
-        let ulElem = document.getElementsByClassName("ul");
-        let inputValue = document.getElementById("input");
-        var todoId;
-        let todoList = document.getElementById("todoList");
+let ulElem = document.getElementsByClassName("ul");
+let inputValue = document.getElementById("input");
+var todoId;
+let todoList = document.getElementById("todoList");
 
-    let todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+let todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
 
-    function updateTodoList() {
+// function updateTodoList() {
+//     if (todoItems.length > 0) {
+//         const curUserTodos = todoItems.filter((item) => item.userId === currentUser.userId)
+//         console.log(curUserTodos); 
+//     }
+//     todoList.innerHTML = "";
+//     curUserTodos.forEach((todo, index) => {
+//         const li = document.createElement("button");
+//         li.setAttribute("class", "btn btn-success")
+//         li.textContent = todo.todoItem;
+
+//         const deleteButton = document.createElement("span");
+//         deleteButton.innerHTML = '<i class="fa-solid fa-trash" style="color: #ed3838;"></i>'
+//         deleteButton.onclick = () => deleteTodo(index);
+
+//         li.appendChild(deleteButton);
+//         todoList.appendChild(li);
+//     });
+// }
+
+function updateTodoList() {
+    const currentUserData = JSON.parse(localStorage.getItem("currentUser"));
+
+    if (currentUserData) {
+        localStorage.setItem("todoItems", JSON.stringify(todoItems));
+        const curUserTodos = todoItems.filter((item) => item.userId === currentUserData.userId);
+
         todoList.innerHTML = "";
-        todoItems.forEach((todo, index) => {
-            const li = document.createElement("button");
-            li.setAttribute("class", "btn btn-success")
-            li.textContent = todo.todoItem;
 
-            const deleteButton = document.createElement("span");
-                deleteButton.innerHTML = '<i class="fa-solid fa-trash" style="color: #ed3838;"></i>'
+        if (curUserTodos.length > 0) {
+            curUserTodos.forEach((todo, index) => {
+                const li = document.createElement("button");
+                li.setAttribute("class", "btn btn-success");
+                li.textContent = todo.todoItem;
+
+                const deleteButton = document.createElement("span");
+                deleteButton.innerHTML = '<i class="fa-solid fa-trash" style="color: #ed3838;"></i>';
                 deleteButton.onclick = () => deleteTodo(index);
-            
+
                 li.appendChild(deleteButton);
                 todoList.appendChild(li);
-        });
-    }
-
-    function addTodo() {
-        const todoItem = inputValue.value.trim();
-        todoId = 100;
-        
-        
-        if (todoItem !== "") {
-            if(Array.isArray(todoItems) && todoItems.length > 0){
-                todoId = todoItems[todoItems.length - 1].todoId + 1;
+            });
+        } else {
+            // Handle the case where the user has no todos
+            deleteAllBtn.disabled = "true";
+            const noTodosMessage = document.createElement("p");
+            noTodosMessage.textContent = "You have no todos.";
+            todoList.appendChild(noTodosMessage);
         }
-        const newTodo = {
+    } else {
+        // Handle the case where there's no current user data
+        console.log("No current user data found");
+    }
+}
+
+
+
+function addTodo() {
+    const todoItem = inputValue.value.trim();
+    todoId = 100;
+
+
+    if (todoItem !== "") {
+        if (Array.isArray(todoItems) && todoItems.length > 0) {
+            todoId = todoItems[todoItems.length - 1].todoId + 1;
+        }
+        newTodo = {
             todoItem,
             todoId,
             userId: JSON.parse(currentUser).userId
         }
         console.log();
-            todoItems.push(newTodo);
-            localStorage.setItem("todoItems", JSON.stringify(todoItems));
-            inputValue.value = "";
-            updateTodoList();
-        }
+        todoItems.push(newTodo);
+        localStorage.setItem("todoItems", JSON.stringify(todoItems));
+        inputValue.value = "";
+        updateTodoList();
+        deleteAllBtn.disabled = false;
     }
-    function deleteTodo(index) {
-        todoItems.splice(index, 1);
+}
+function deleteTodo(index) {
+    todoItems.splice(index, 1);
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    updateTodoList();
+}
+
+function deleteAll() {
+    todoItems.length = 0;
+    localStorage.removeItem("todoItems")
+    updateTodoList();
+}
+
+function showList () {
         localStorage.setItem("todoItems", JSON.stringify(todoItems));
         updateTodoList();
-    }
+        deleteAllBtn.disabled = false;
+        showAllBtn.style.display = "none";
+}
 
-    function deleteAll (){
-        todoItems.length = 0;
-        localStorage.removeItem("todoItems")
-        updateTodoList();
-        }
+const changePage = () => {
+    window.location.href = 'home.html';
+    updateTodoList();
+}
 
-    // todoItems? todoItems.map(item, (item) => {
-    //     console.log(item);
-    // })
+const logOut = () =>{
+    localStorage.removeItem("currentUser");
+    window.location.href = "index.html";    
+}
+
+// todoItems? todoItems.map(item, (item) => {
+//     console.log(item);
+// })
+
+
+// const currentTodos = () => {
+//     const currentUserTodo = todoItems && todoItems.filter((item) => item.userId == currentUser.userId)
+//     console.log(currentUserTodo);
+//     // updateTodoList(currentUserTodo);
+// }
